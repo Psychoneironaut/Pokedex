feef72c8e297f86b5613313f64af1ba7a30a9fea
Psychoneironaut <willmabreyv@gmail.com>
2017-03-02 11:50:22 -0600

    Added Next 20 & Previous 20 buttons
    Added buttons that allow the user to display the next 20 Pokemon in the
    Pokedex, and the previous 20 Pokemon in the Pokedex.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,23 @@
 angular.module('myApp', ['pokedex', 'ui.bootstrap']);
 
 angular.module('myApp').controller('myController', ['$scope', '$http', 'Pokemon', function ($scope, $http, Pokemon) {
-        Pokemon.all($http).then(function(result){
+    $scope.offset = 0;
+    
+    Pokemon.all($http).then(function (result) {
         $scope.model = result.data.results;
     })
+    
+    $scope.nextPage = function () {
+        $scope.offset += 20;
+        Pokemon.all($http, $scope.offset).then(function (result) {
+        $scope.model = result.data.results;
+        })
+    }
+    
+    $scope.prevPage = function () {
+        $scope.offset -= 20;
+        Pokemon.all($http, $scope.offset).then(function (result) {
+        $scope.model = result.data.results;
+        })
+    }
 }]);
--- scripts/modules/pokedex.js
+++ scripts/modules/pokedex.js
@@ -2,9 +2,11 @@ angular.module("pokedex", []);
 
 angular.module("pokedex").factory("Pokemon", function PokemonFactory(){
     var url = "http://pokeapi.co/api/v2/pokemon/";
-    var offset = 0;
     return {
-        all : function($http){
+        all : function($http, offset){
+            if(!offset){
+                offset = 0;
+            }
             return $http.get(url + "?offset=" + offset);
         }
     }
